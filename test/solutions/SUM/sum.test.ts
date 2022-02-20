5f92c03946242a4979ff5d197401ea84e7b1b150
sbx_user1051 <sbx_user1051@169.254.16.197>
2022-02-20 23:45:01 +0000

    Sun Feb 20 23:45:01 UTC 2022

--- test/solutions/SUM/sum.test.ts
+++ test/solutions/SUM/sum.test.ts
@@ -1,10 +1,12 @@
+import {sum} from "../../../src/solutions/SUM/sum";
+
 describe('SUM challenge: adding two numbers', () => {
 	describe("Given the inputs 1 and 2", () => {
 		const firstInput = 1;
 		const secondInput = 2;
 
 		it('should return 3', () => {
-			assert.equal(sum(firstInput, secondInput), 3);
+			expect(sum(firstInput, secondInput)).toEqual(3);
 		});
 	});
 });
