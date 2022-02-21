044b2500309997fe0ddca7546f3a44d0fec11e0c
sbx_user1051 <sbx_user1051@169.254.16.197>
2022-02-21 02:01:05 +0000

    Mon Feb 21 02:01:05 UTC 2022

--- src/solutions/CHK/checkout.ts
+++ src/solutions/CHK/checkout.ts
@@ -5,14 +5,23 @@ export = (SKUs: string) => {
     C: 20,
     D: 15
   };
-
   const skuInput = SKUs.split("");
+
+  const basket:Record<string, number> = {}
+
+
   let total = 0;
 
   for (const sku of skuInput) {
     if (!(sku in priceMapping)) return -1;
+    if(!(sku in basket)) basket[sku] = 0;
+
+    basket[sku]++;
     total = total + priceMapping[sku];
+
+    if(basket?.A % 3 === 0) total -= 20;
   }
 
   return total;
 };
+
--- test/solutions/CHK/checkout.test.ts
+++ test/solutions/CHK/checkout.test.ts
@@ -39,6 +39,9 @@ describe("Given a customer buys a list of items", () => {
     describe("When they are eligible for special offers", () => {
         describe.each([
             {SKUs: "AAA", total: 130},
+            {SKUs: "AAAA", total: 180},
+            {SKUs: "AAAAA", total: 240},
+            {SKUs: "AAAAA", total: 260},
         ])("And they buy $SKUs", ({SKUs, total}) => {
             it(`should total to ${total}`, () => {
                 expect(checkout(SKUs)).toEqual(total);
@@ -52,4 +55,5 @@ describe("Given a customer buys a list of items", () => {
     });
   });
 });
+
 
