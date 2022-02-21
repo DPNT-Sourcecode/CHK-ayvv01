b55e8985cfaa0ae68ef3e7ed588c5ef97b9b8378
sbx_user1051 <sbx_user1051@169.254.16.197>
2022-02-21 03:46:36 +0000

    Mon Feb 21 03:46:36 UTC 2022

--- src/solutions/CHK/checkout.ts
+++ src/solutions/CHK/checkout.ts
@@ -19,95 +19,103 @@ interface GiveawayOffer {
   itemToGiveFree: keyof PriceMapping;
 }
 
-type Basket = Record<string, number>
+type Basket = Record<string, number>;
 
 const priceMapping: PriceMapping = {
-    A: {
-        price: 50,
-        offer: [
-            {
-                quantity: 3,
-                amountToDeduct: 20
-            },
-            {
-                quantity: 5,
-                amountToDeduct: 50
-            }
-        ]
-    },
-    B: {
-        price: 30,
-        offer: [
-            {
-                quantity: 2,
-                amountToDeduct: 15
-            }
-        ]
-    },
-    C: {
-        price: 20
-    },
-    D: {
-        price: 15
-    },
-    E: {
-        price: 40
-    },
-    F: {
-        price: 10
-    }
+  A: {
+    price: 50,
+    offer: [
+      {
+        quantity: 3,
+        amountToDeduct: 20
+      },
+      {
+        quantity: 5,
+        amountToDeduct: 50
+      }
+    ]
+  },
+  B: {
+    price: 30,
+    offer: [
+      {
+        quantity: 2,
+        amountToDeduct: 15
+      }
+    ]
+  },
+  C: {
+    price: 20
+  },
+  D: {
+    price: 15
+  },
+  E: {
+    price: 40
+  },
+  F: {
+    price: 10,
+    offer: [
+      {
+        quantity: 3,
+        amountToDeduct: 10
+      }
+    ]
+  }
 };
 const freeMapping: FreeMapping = {
-    E: {
-        quantity: 2,
-        itemToGiveFree: "B"
-    }
+  E: {
+    quantity: 2,
+    itemToGiveFree: "B"
+  }
 };
 
 const removeFree = (basket: Basket, subtotal: number): [Basket, number] => {
-    for(const sku of Object.keys(basket)){
-        if(freeMapping[sku]){
-            const {quantity, itemToGiveFree} = freeMapping[sku];
-            if(basket[itemToGiveFree] && basket[itemToGiveFree] > 0) {
-                const amountOfFreeToDeduct = Math.floor(basket[sku] / quantity);
-                if(amountOfFreeToDeduct !== 0) {
-                    if (amountOfFreeToDeduct <= basket[sku]) {
-                        basket[itemToGiveFree] -= amountOfFreeToDeduct;
-                        subtotal -= priceMapping[itemToGiveFree].price * amountOfFreeToDeduct;
-                    } else {
-                        subtotal -= priceMapping[itemToGiveFree].price * basket[itemToGiveFree];
-                        basket[itemToGiveFree] = 0
-                    }
-                }
-            }
+  for (const sku of Object.keys(basket)) {
+    if (freeMapping[sku]) {
+      const { quantity, itemToGiveFree } = freeMapping[sku];
+      if (basket[itemToGiveFree] && basket[itemToGiveFree] > 0) {
+        const amountOfFreeToDeduct = Math.floor(basket[sku] / quantity);
+        if (amountOfFreeToDeduct !== 0) {
+          if (amountOfFreeToDeduct <= basket[sku]) {
+            basket[itemToGiveFree] -= amountOfFreeToDeduct;
+            subtotal -=
+              priceMapping[itemToGiveFree].price * amountOfFreeToDeduct;
+          } else {
+            subtotal -=
+              priceMapping[itemToGiveFree].price * basket[itemToGiveFree];
+            basket[itemToGiveFree] = 0;
+          }
         }
+      }
     }
-    return [basket, subtotal];
-}
+  }
+  return [basket, subtotal];
+};
 
 const applyOffers = (basket: Record<string, number>, total: number) => {
-    for (const sku of Object.keys(basket)) {
-        if (priceMapping[sku].offer && priceMapping[sku].offer!.length > 0) {
-            let hasOffersLeft = true;
-            while (hasOffersLeft) {
-                const offersThatApply: MultiBuyOffer[] = priceMapping[sku].offer!.filter(
-                    offer => Math.floor(basket[sku] / offer.quantity) > 0
-                );
+  for (const sku of Object.keys(basket)) {
+    if (priceMapping[sku].offer && priceMapping[sku].offer!.length > 0) {
+      let hasOffersLeft = true;
+      while (hasOffersLeft) {
+        const offersThatApply: MultiBuyOffer[] = priceMapping[
+          sku
+        ].offer!.filter(offer => Math.floor(basket[sku] / offer.quantity) > 0);
 
-                if (offersThatApply.length > 0) {
-                    const offerToUse = offersThatApply[offersThatApply.length - 1];
+        if (offersThatApply.length > 0) {
+          const offerToUse = offersThatApply[offersThatApply.length - 1];
 
-                    if (offerToUse) {
-                        total -= offerToUse.amountToDeduct;
-                        basket[sku] -= offerToUse.quantity;
-                    }
-                } else hasOffersLeft = false;
-            }
-        }
+          if (offerToUse) {
+            total -= offerToUse.amountToDeduct;
+            basket[sku] -= offerToUse.quantity;
+          }
+        } else hasOffersLeft = false;
+      }
     }
+  }
 
-    return total;
-}
+  return total;
+};
 
 export = (SKUs: string) => {
   const skuInput = SKUs.split("");
@@ -123,5 +131,6 @@ export = (SKUs: string) => {
     basket[sku]++;
     subtotal = subtotal + priceMapping[sku].price;
   }
-    return applyOffers(...removeFree(basket, subtotal))
+  return applyOffers(...removeFree(basket, subtotal));
 };
+
