3d04acbf54460df759d6b4c30ff16c15d8f36973
sbx_user1051 <sbx_user1051@169.254.16.197>
2022-02-21 01:49:05 +0000

    Mon Feb 21 01:49:05 UTC 2022

--- src/solutions/CHK/checkout.ts
+++ src/solutions/CHK/checkout.ts
@@ -1,7 +1,7 @@
 export = (SKUs: string) => {
   const priceMapping: Record<string, number> = {
-    A: 50,
-    B: 30
+    "A": 50,
+    "B": 30
   };
 
   const skuInput = SKUs.split("");
@@ -9,13 +9,11 @@ export = (SKUs: string) => {
   let total = 0;
 
   for (const sku of skuInput) {
-      console.log(Object.keys(priceMapping))
-      console.log(sku)
-      console.log(sku in Object.keys(priceMapping))
-      if (!(sku in Object.keys(priceMapping))) return -1;
+      if (!(priceMapping.hasOwnProperty()) return -1;
     total = total + priceMapping[sku]
   }
 
   return total;
 };
+
 
