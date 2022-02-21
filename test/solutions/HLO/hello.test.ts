f1e73be06ab1cd607f71b217417cb9a9bf8ea27a
sbx_user1051 <sbx_user1051@169.254.16.197>
2022-02-21 01:08:44 +0000

    Mon Feb 21 01:08:44 UTC 2022

--- test/solutions/HLO/hello.test.ts
+++ test/solutions/HLO/hello.test.ts
@@ -1,7 +1,15 @@
 import hello from "../../../src/solutions/HLO/hello"
 
-describe('Given the function hello is run with an string empty parameter',  () => {
-    it('should return "Hello, World!"', () => {
-        expect(hello("")).toEqual("Hello, World!");
-    })
+describe('Given the function hello is run',  () => {
+    describe("When it is called with an empty parameter", () => {
+        it('should return "Hello, World!"', () => {
+            expect(hello("")).toEqual("Hello, World!");
+        })
+    });
+
+    describe.each(["Mark", "Jane", "John", "Patricia"])("When it is called with the name %s", (name) => {
+        it(`should return "Hello, ${name}!`, () => {
+            expect(hello(name)).toEqual(`Hello, ${name}!`);
+        })
+    });
 });
