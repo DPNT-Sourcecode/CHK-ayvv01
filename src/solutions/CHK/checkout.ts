7b75575ad217665383f0920fae2a26514ecde8b2
sbx_user1051 <sbx_user1051@169.254.16.197>
2022-02-21 02:07:05 +0000

    Mon Feb 21 02:07:05 UTC 2022

--- src/solutions/CHK/checkout.ts
+++ src/solutions/CHK/checkout.ts
@@ -1,9 +1,22 @@
+interface PriceMapping {
+    [key: string]: {
+        price: number,
+        offer?: {
+            quantity: number,
+            specialPrice: number
+        }
+    }
+}
+
 export = (SKUs: string) => {
-  const priceMapping: Record<string, number> = {
-    A: 50,
-    B: 30,
-    C: 20,
-    D: 15
+  const priceMapping: PriceMapping = {
+    A: {
+        price: 50
+    },
+    B: {
+        price:30},
+    C: {price:20},
+    D: {price: 15}
   };
   const skuInput = SKUs.split("");
 
@@ -17,11 +30,12 @@ export = (SKUs: string) => {
     if(!(sku in basket)) basket[sku] = 0;
 
     basket[sku]++;
-    total = total + priceMapping[sku];
+    total = total + priceMapping[sku].price;
 
     if(basket?.A % 3 === 0) total -= 20;
   }
 
   return total;
 };
+
 
