f0f305691d76afa8a59509959fde98800f393d85
sbx_user1051 <sbx_user1051@169.254.16.197>
2022-02-21 01:37:05 +0000

    Mon Feb 21 01:37:05 UTC 2022

--- src/solutions/CHK/checkout.ts
+++ src/solutions/CHK/checkout.ts
@@ -1,5 +1,16 @@
 export = (SKUs: string) => {
+    const priceMapping = {
+        "A": 50,
+        "B": 30
+    }
+    const inputSKUs = SKUs.split("");
+
     let total = 0;
-    SKUs.t
-    return SKUs.length * 50;
+
+    for(const sku of inputSKUs){
+        if(sku === "A") total += priceMapping.A;
+        if(sku === "B") total += priceMapping.B;
+    }
+
+    return total;
 };
